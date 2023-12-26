1b576a6e3fddeaf61852f6193ce1ce9b5e6785af
OPr1me <ztv8556@gmail.com>
2023-12-26 20:34:12 +0300

    Код для turtle

---
+++ my_turtle.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtlePen = turtle.Turtle()
+window = turtle.Screen()
+...
+
+turtlePen.color('green')
+def polygon(n, size=120):
+    if n > 2:
+        angle = 360 / n
+
+        for n in range(0, n):
+            turtlePen.left(angle)
+            turtlePen.forward(size)
+
+
+turtlePen.speed(60)
+
+for i in range(0, 100, 5):
+    polygon(3, 10 + i)
+    turtlePen.left(20)
+
+...
+window.mainloop()
